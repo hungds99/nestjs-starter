2ec5c39104f2fdcc09c57504d25fb13af77a472c
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-02-28 11:45:54 +0700

    :rocket: init source template

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: new Logger(),
+  });
+  await app.listen(3000);
+  Logger.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
