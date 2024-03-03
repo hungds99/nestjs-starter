c61fdf3398b06e833639c7e1e496e9bdd32529ae
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-03-03 11:56:18 +0700

    Refactor LoggerMiddleware and update import paths - Organizing middleware and improving code structure.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
+import { PrismaClientExceptionFilter } from './common/filters/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -18,6 +18,9 @@ async function bootstrap() {
   // Config filter
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  // Config logger
+  app.useLogger(new Logger());
 
   await app.listen(3000);
   Logger.log(`Application is running on: ${await app.getUrl()}`);
